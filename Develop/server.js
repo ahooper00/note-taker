6273bd2b805847d54ae34f4c4181ecfdfa3d951a
Amelia Hooper <ajhooper97@gmail.com>
2021-11-08 17:57:38 +1100

    saves new notes, retrieves old notes, deletes notes

--- Develop/server.js
+++ Develop/server.js
@@ -6,23 +6,60 @@ const PORT = 3001;
 
 const app = express();
 
+app.use(express.static('public'));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
 async function getNotes() {
     const database = await fs.readFile(path.join(__dirname, '/db/db.json'));
     return JSON.parse(database);
-}
+};
 
 // GET route
+// Gets the index.html (front page)
 app.get('/', (req, res) =>
     res.sendFile(path.join(__dirname, '/public/index.html'))
 );
 
+// Gets the notes.html page where the user can interact with notes
 app.get('/notes', (req, res) =>
     res.sendFile(path.join(__dirname, '/public/notes.html'))
 );
 
+// Gets the saved notes in the db.json file
 app.get('/api/notes', async (req, res) => {
     const data = await getNotes();
     res.json(data)
+});
+
+// POST route
+app.post('/api/notes', async (req, res) => {
+    let newNote = req.body;
+    let noteList = await getNotes();
+    let noteLength = (noteList.length).toString();
+
+    newNote.id = noteLength;
+
+    noteList.push(newNote)
+
+    // add a new note
+    console.log(newNote);
+
+    fs.writeFile("./db/db.json", JSON.stringify(noteList));
+    res.json(noteList);
+});
+
+// DELETE route
+app.delete('/api/notes/:id', async (req, res) => {
+    let noteList = await getNotes();
+    let noteId = (req.params.id).toString();
+
+    noteList = noteList.filter(deleted => {
+        return deleted.id != noteId;
+    });
+
+    fs.writeFile("./db/db.json", JSON.stringify(noteList));
+    res.json(noteList);
 });
 
 app.listen(PORT, () =>
