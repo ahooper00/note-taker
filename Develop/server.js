250ed15c267511d0ea650f5a6f163e13825eeb3a
Amelia Hooper <ajhooper97@gmail.com>
2021-11-09 14:13:09 +1100

    added readme

--- Develop/server.js
+++ Develop/server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const path = require('path');
 const fs = require('fs/promises');
 
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 
 const app = express();
 
@@ -63,4 +63,4 @@ app.delete('/api/notes/:id', async (req, res) => {
 });
 
 app.listen(PORT, () =>
-    console.log(`Listening at http://localhost:${PORT}`));
+    console.log(`Server listening on PORT ${PORT}`));
